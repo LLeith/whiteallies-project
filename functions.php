5eb378d1deb6980dac7da3f6dad12b7ab29a3b86
LLeith <58423745+LLeith@users.noreply.github.com>
2021-01-31 13:45:45 +0000

    Revert "Made the homepage slider content-managed, introduced post loop to arc…"

--- functions.php
+++ functions.php
@@ -1,14 +1,12 @@
-<?php
+<?php 
 
 function whiteallie_theme_support(){
 // Adds dynamic title tag support
 add_theme_support('title-tag');
-// Adds ability for admin to update Logo
+// Adds ability for admin to update Logo 
 add_theme_support('custom-logo');
 // ability to add a thumbnail as a featured image to posts
 add_theme_support('post-thumbnails');
-// ability to add Excerpts to pages
-add_post_type_support('page', 'excerpt');
 }
 add_action('after_setup_theme', 'whiteallie_theme_support');
 
@@ -18,7 +16,6 @@ add_action('after_setup_theme', 'whiteallie_theme_support');
 function whiteallie_menus() {
   $locations = array(
     'primary' => "Desktop primary top nav",
-    'secondary' => "Action Allies (front page)",
     'footer' => "Footer menu items"
   );
   register_nav_menus($locations);
@@ -30,7 +27,7 @@ add_action('init','whiteallie_menus');
 
 // enqueue styles
 function whiteallie_register_styles(){
-
+  
   $version = wp_get_theme()->get( 'Version' );
   wp_enqueue_style('whiteallie-style', get_template_directory_uri() . "/style.css", array('whiteallie-bootstrap'), $version, 'all');
   wp_enqueue_style('whiteallie-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css", array(), '4.5.3', 'all');
@@ -41,10 +38,10 @@ add_action( 'wp_enqueue_scripts', 'whiteallie_register_styles');
 
 
 // enqueue scripts
-function whiteallie_register_scripts(){
+function whiteallie_register_scripts(){  
   wp_enqueue_script('whiteallie-jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), '3.5.1',true);
   wp_enqueue_script('whiteallie-popper', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array(), '4.5.3',true);
-  wp_enqueue_script('whiteallie-jsicons', 'https://unpkg.com/ionicons@5.1.2/dist/ionicons.js', array(), '5.1.2',true);
+  wp_enqueue_script('whiteallie-jsicons', 'https://unpkg.com/ionicons@5.1.2/dist/ionicons.js', array(), '5.1.2',true);  
 }
 add_action( 'wp_enqueue_scripts', 'whiteallie_register_scripts');
 
@@ -56,67 +53,13 @@ function whiteallie_widget_areas(){
       'before_title' => '<h2>',
       'after_title' => '</h2>',
       'before_widget' => '',
-      'after_widget' => '<p>White Allies &copy; '.date('Y').'</p>',
+      'after_widget' => '',
       'name' => 'Footer Area',
       'id' => 'footer-1',
       'description' => 'Footer Widget area'
     )
   );
-  register_sidebar(
-    array(
-      'before_title' => '<h2>',
-      'after_title' => '</h2>',
-      'before_widget' => '<article id="our-mission">',
-      'after_widget' => '</article>',
-      'name' => 'Mission',
-      'id' => 'mission',
-      'description' => 'Put your Mission Statement here'
-    )
-  );
 }
 add_action( 'widgets_init', 'whiteallie_widget_areas');
 
-// Adds a Custom Post Type to use for the Slider in the front page template
-function waslider_custom_post_type() {
-    register_post_type('wa_slider',
-        array(
-            'labels'      => array(
-                'name'          => __('Slides', 'textdomain'),
-                'singular_name' => __('Slide', 'textdomain'),
-            ),
-                'public'      => true,
-                'has_archive' => true,
-                'menu_icon' => 'dashicons-format-gallery',
-        )
-    );
-}
-add_action('init', 'waslider_custom_post_type');
-add_post_type_support( 'wa_slider', 'thumbnail' );
-
-<<<<<<< HEAD
-
-// Add page excerpt to menu items for Action Allies menu
-function wa_nav_menu_page_excerpts( $title, $item, $args, $depth ) {
-  if ( $args->menu->name == 'Action Allies Menu' ) {
-    $pid = $item->object_id;
-    $text = get_the_excerpt($pid);
-    if ( !empty($text) ) {
-      $title .= ' <span class="excerpt">- '.$text.'</span>';
-    }
-  }
-  return $title;
-}
-add_filter( 'nav_menu_item_title', 'wa_nav_menu_page_excerpts', 10, 4 );
-
-// Add custom class to menu items in Action Allies menu
-function wa_menu_classes( $classes, $item, $args ) {
-  if ( $args->menu->name == 'Action Allies Menu' ) {
-    $classes[] = 'action-btn';
-  }
-  return $classes;
-}
-add_filter( 'nav_menu_css_class', 'wa_menu_classes', 1, 3 );
-?>
-=======
  ?>
->>>>>>> ed24093bf03a1b21981dcf7e7b50782c451cd003
--- template-parts/template-article.php
+++ template-parts/template-article.php
@@ -1,15 +1,15 @@
 <div class="container">
 				<header class="content-header">
 					<div class="meta mb-3"><span class="date"><?php the_date(); ?></span>
-            <?php
+            <?php  
               the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span.');
             ?>
-
+            
             <span class="tag"><i class="fa fa-tag"></i> category</span>
           </div>
 				</header>
-
-        <?php
+        
+        <?php 
         the_content(); 
         ?>
 </div>
