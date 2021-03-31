876ce261ad4aeffa5c517e3e8e4edce5fbe4b81a
Michael Adamson <michael.j.adamson@virginmeida.com>
2021-03-31 15:49:23 +0100

    jquery changes for responsive menu

--- assets/js/responsive-menu.js
+++ assets/js/responsive-menu.js
@@ -1,15 +1,3 @@
-// $('.mobile-menu-toggle').click(function(e){
-//   console.log($(this));
-//     $(this).toggleClass('expanded-menu-toggle');
-//     $(this).parent().find('.menu').toggleClass('expanded-mobile-menu');
-//     $(this).parent().toggleClass('nav-expanded');
-//     // if($(this).parent().find('.menu').hasClass('expanded-mobile-menu')){
-//     //   $(this).removeClass('expanded-menu-toggle').parent().removeClass('nav-expanded');
-//     // }else{
-//     //   $(this).addClass('expanded-menu-toggle').parent().addClass('nav-expanded').find('.menu').addClass('expanded-mobile-menu');
-//     // }
-// });
-
 const $menu = $('#mobile-nav');
 
 $(document).mouseup(e => {
