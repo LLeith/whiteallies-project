7a4e25495ad7f23ebdfb1855ba74367dcb6807f8
Michael Adamson <michael.j.adamson@virginmeida.com>
2021-03-31 15:47:25 +0100

    improvement to mobile menu behaviour (although still not quite right)

--- assets/js/responsive-menu.js
+++ assets/js/responsive-menu.js
@@ -1,7 +1,25 @@
-$('.mobile-menu-toggle').click(function(e){
-    if($(this).parent().find('.menu').hasClass('expanded-mobile-menu')){
-      $(this).removeClass('expanded-menu-toggle').parent().removeClass('nav-expanded').find('.menu').removeClass('expanded-mobile-menu');
-    }else{
-      $(this).addClass('expanded-menu-toggle').parent().addClass('nav-expanded').find('.menu').addClass('expanded-mobile-menu');
-    }
+// $('.mobile-menu-toggle').click(function(e){
+//   console.log($(this));
+//     $(this).toggleClass('expanded-menu-toggle');
+//     $(this).parent().find('.menu').toggleClass('expanded-mobile-menu');
+//     $(this).parent().toggleClass('nav-expanded');
+//     // if($(this).parent().find('.menu').hasClass('expanded-mobile-menu')){
+//     //   $(this).removeClass('expanded-menu-toggle').parent().removeClass('nav-expanded');
+//     // }else{
+//     //   $(this).addClass('expanded-menu-toggle').parent().addClass('nav-expanded').find('.menu').addClass('expanded-mobile-menu');
+//     // }
+// });
+
+const $menu = $('#mobile-nav');
+
+$(document).mouseup(e => {
+   if (!$menu.is(e.target) // if the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   {
+     $menu.removeClass('expanded-mobile-menu');
+  }
+ });
+
+$('#hamburger').on('click', () => {
+  $menu.toggleClass('expanded-mobile-menu');
 });
