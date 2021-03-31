9b68ce5db72f7ea2c41c4b34915cf2a1af863682
Michael Adamson <michael.j.adamson@virginmeida.com>
2021-03-31 13:00:17 +0100

    Corrected margins on default pages Added post IDs to story titles

--- archive.php
+++ archive.php
@@ -23,7 +23,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <a href="<?php the_permalink(); ?>">
   <div class="box">
-    <h2><?php the_title(); ?></h2>
+    <h2><?php the_ID(); ?>: <?php the_title(); ?></h2>
     <p><?php the_excerpt(); ?></p>
   </div>
 </a>
